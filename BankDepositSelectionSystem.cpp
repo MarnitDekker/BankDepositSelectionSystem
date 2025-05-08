cb79b96d909c6e9734344dbd7c257c86e91eecfa
MarnitDekker <fateeva.marina2005@yandex.ru>
2025-05-08 22:28:21 +0300

    Initial commit

---
+++ BankDepositSelectionSystem.cpp
@@ -0,0 +1,12 @@
+﻿// BankDepositSelectionSystem.cpp: определяет точку входа для приложения.
+//
+
+#include "BankDepositSelectionSystem.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
